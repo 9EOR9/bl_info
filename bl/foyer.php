eeca22297d1839998930b851401cd9b894e9e76b
Georg Richter <georg@mariadb.com>
2026-04-23 12:40:01 +0200

    fix fonts

--- bl/foyer.php
+++ bl/foyer.php
@@ -6,6 +6,9 @@ require_once 'runde.php';
 <head>
     <meta charset="UTF-8">
     <title>WBH Infoscreen</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="foyer.css">
 </head>
 <body>
